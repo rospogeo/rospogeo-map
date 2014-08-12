6270d138847aa1c6021f35b9cfaff48ed4e5da04
danieljoppi <daniel.joppi@gmail.com>
2014-08-12 17:13:59 -0300

    test with directive layers

--- src/main/directive/geo-directive-layer.js
+++ src/main/directive/geo-directive-layer.js
@@ -7,21 +7,37 @@
 
     app.directive('layers', [function() {
         return {
-            restrict: 'EA',
-            replace: true,
+            require: '^map',
+            restrict: 'E',
+            transclude: true,
+            template: '<div class="layers"></div>',
             controller: ["$scope", function($scope) {
 
-            }]
+            }],
+            link: function(scope, element, attrs, crtl) {
+                console.log(crtl);
+                crtl.addLayer();
+            }
         };
     }]);
 
     app.directive('layer', [function() {
         return {
-            restrict: 'EA',
-            replace: true,
+            require: ['^layers', '^map'],
+            restrict: 'E',
+            scope: {
+                title: '@'
+            },
+            transclude: true,
+            template: '<div class="layer"></div>',
             controller: ["$scope", function($scope) {
 
-            }]
+            }],
+            link: function(scope, element, attrs, crtls) {
+                console.log(crtls);
+                var mapCrtl = crtls[1];
+                mapCrtl.addLayer();
+            }
         };
     }]);
 })(angular, geoMap);
--- src/main/directive/geo-directive-map.js
+++ src/main/directive/geo-directive-map.js
@@ -7,27 +7,38 @@
 
     app.directive('map', [function() {
         return {
-            restrict: 'EA',
+            restrict: 'E',
             scope: {
                 id: "@",
                 width: "@",
                 height: "@"
             },
             replace: true,
+            transclude: true,
             //templateUrl: 'partials/geo-directive-map.tpl.html',
             template: '<div  class="smallmap"></div>',
             controller: ["$scope", function($scope) {
                 $scope.lon = 5;
                 $scope.lat = 40;
                 $scope.zoom = 5;
+
+                this.addLayer = function(){
+                    if (!$scope.map) {
+                        console.error("map not found");
+                        return;
+                    }
+                    console.log("layer");
+                    var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+                    $scope.map.addLayer(layer);
+                };
             }],
             link: function (scope, element, attributes) {
                 scope.map = new OpenLayers.Map( scope.id );
-                scope.layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
-                scope.map.addLayer(scope.layer);
+                //scope.layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+                //scope.map.addLayer(scope.layer);
 
-                scope.map.setCenter(new OpenLayers.LonLat(scope.lon, scope.lat), scope.zoom);
-                scope.map.addControl( new OpenLayers.Control.LayerSwitcher() );
+                //scope.map.setCenter(new OpenLayers.LonLat(scope.lon, scope.lat), scope.zoom);
+                //scope.map.addControl( new OpenLayers.Control.LayerSwitcher() );
             }
         };
     }]);
